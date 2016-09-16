264efc374481d26e3b69e1fc0752222ab80e0bca
Yusuke Kuoka <ykuoka@gmail.com>
2016-09-16 11:55:16 +0900

    Add missing files

---
+++ cmd/env.go
@@ -0,0 +1,46 @@
+// Copyright © 2016 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	env "../env"
+	subcommands "./env"
+)
+
+var EnvCmd = &cobra.Command{
+	Use:   "env",
+	Short: "Print currently selected environment",
+	Long: `Print currently selected environment. The environment can be via the command "var env set" or "var env switch".
+
+Example:
+var env switch dev
+var env #=> Prints "dev"
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		env, err := env.Get()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(env)
+	},
+}
+
+func init() {
+	EnvCmd.AddCommand(subcommands.SetCmd)
+}
---
+++ env/env.go
@@ -0,0 +1,67 @@
+package env
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	log "github.com/Sirupsen/logrus"
+
+	"github.com/juju/errors"
+)
+
+type EnvFile struct {
+	appName string
+}
+
+var e *EnvFile
+
+func init() {
+	SetAppName("var")
+}
+
+func New(name string) *EnvFile {
+	e := new(EnvFile)
+	e.appName = name
+
+	return e
+}
+
+func SetAppName(name string) {
+	e = New(name)
+}
+
+func GetPath() string { return e.GetPath() }
+func (e *EnvFile) GetPath() string {
+	return fmt.Sprintf(".%senv", e.appName)
+}
+
+func Set(env string) error { return e.Set(env) }
+func (e *EnvFile) Set(env string) error {
+	err := ioutil.WriteFile(e.GetPath(), []byte(env), 0644)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	return nil
+}
+
+func Get() (string, error) { return e.Get() }
+func (e *EnvFile) Get() (string, error) {
+	env, err := ioutil.ReadFile(e.GetPath())
+	if err != nil {
+		return "", errors.Trace(err)
+	}
+	return string(env), nil
+}
+
+func GetOrSet(defaultEnv string) (string, error) { return e.GetOrSet(defaultEnv) }
+func (e *EnvFile) GetOrSet(defaultEnv string) (string, error) {
+	env, err := e.Get()
+	if err != nil {
+		log.Errorf("%s", err)
+		err := e.Set(defaultEnv)
+		if err != nil {
+			return "", errors.Trace(err)
+		}
+	}
+	return env, nil
+}
