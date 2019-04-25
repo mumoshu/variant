a700c20eacf78d78c66b3f1a62e8798188f05f67
Alexander Tolstikov <tolstikov@gmail.com>
2019-04-25 09:48:19 +0300

    Add custom log colors

--- pkg/variant_text_log_formatter.go
+++ pkg/variant_text_log_formatter.go
@@ -8,17 +8,11 @@ import (
 
 type variantTextFormatter struct {
 	colorize *colorstring.Colorize
+	colors map[logrus.Level]string
 }
 
 func (f *variantTextFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-	var prefix string
-	if entry.Level < logrus.InfoLevel {
-		prefix = "[red]"
-	} else if entry.Level < logrus.DebugLevel {
-		prefix = "[cyan]"
-	} else {
-		prefix = "[dark_gray]"
-	}
+	var prefix = "[" + f.colors[entry.Level] + "]"
 	app := entry.Data["app"]
 	if app != nil {
 		switch app := app.(type) {
